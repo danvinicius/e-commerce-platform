e402f8c66abd528c5f036de52cddd4c95aed1739
Daniel Vinícius <viniccius774@gmail.com>
2024-05-18 21:03:06 -0300

    feat: add product repository

---
+++ src/main/java/com/danvinicius/ecommerce/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.danvinicius.ecommerce.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.danvinicius.ecommerce.entities.product.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+    
+}
